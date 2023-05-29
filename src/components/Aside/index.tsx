69ee7c48466a2c25b92aea433308903e19daafd4
pauloWeslleY <weslleyspfc@13outlook.com>
2023-05-29 20:29:12 -0300

    feat: Create sidebar and menu items

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { Container } from './styles'
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from 'react-icons/md'
+import {
+  Container,
+  Header,
+  Logotipo,
+  MenuHero,
+  MenuHeroItems,
+  Title,
+} from './styles'
+
+import Logo from '../../assets/logo.svg'
 
 export const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <Logotipo src={Logo} alt={'Logotipo do App'} />
+        <Title>Minha Carteira</Title>
+      </Header>
+
+      <MenuHero>
+        <MenuHeroItems href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuHeroItems>
+        <MenuHeroItems href="#">
+          <MdArrowDownward /> Entradas
+        </MenuHeroItems>
+        <MenuHeroItems href="#">
+          <MdArrowUpward />
+          Saídas
+        </MenuHeroItems>
+        <MenuHeroItems href="#">
+          <MdExitToApp />
+          Sair
+        </MenuHeroItems>
+      </MenuHero>
     </Container>
   )
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,6 +3,51 @@ import styled from 'styled-components'
 export const Container = styled.div`
   grid-area: Aside;
 
-  color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.secondary};
+
+  padding-left: 20px;
+  border-right: 1px solid ${(props) => props.theme.colors.gray};
+`
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+
+  height: 70px;
+`
+
+export const Title = styled.h3`
+  color: ${(props) => props.theme.colors.white};
+  margin-left: 10px;
+`
+
+export const Logotipo = styled.img`
+  height: 40px;
+  width: 40px;
+`
+
+export const MenuHero = styled.nav`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+`
+
+export const MenuHeroItems = styled.a`
+  color: ${(props) => props.theme.colors.info};
+  text-decoration: none;
+
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  transition: ease-in-out 0.3s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  > svg {
+    font-size: 20px;
+  }
 `
